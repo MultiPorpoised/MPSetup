65f7522f001e66545e6dcfb18f49f8ec220a770d
MultiPorpoised <multiporpoised@gmail.com>
2021-08-15 20:58:27 -0700

    Add .tmux.conf

--- setmeup.sh
+++ setmeup.sh
@@ -5,14 +5,16 @@ pushd "${BASH_SOURCE%/*}/" > /dev/null || exit
 # TODO: install necessary packages
 
 # TODO: set up other dotfiles
-# Set .vimrc to be the one from this project.
-# Does not modify the file if it already exists.
-if [ -e ~/.vimrc ]; then
-    echo "~/.vimrc already exists. If you wish to overwrite it, first remove it."
-else
-    echo "softlinking .vimrc"
-    ln -s $(pwd)/.vimrc ~/.vimrc
-fi
+# Set .vimrc and .tmux.conf to be the ones from this project.
+# Does not modify the files if they already exist.
+for FILE in .vimrc .tmux.conf
+    do if [ -e ~/$FILE ]; then
+        echo "~/$FILE already exists. If you wish to overwrite it, first remove it."
+    else
+        echo "softlinking $FILE"
+        ln -s $(pwd)/$FILE ~/$FILE
+    fi
+done
 
 # Return working directory to previous state
 popd > /dev/null
